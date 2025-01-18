7dc25a7fbb7ac142042d5ef2460a01bed8bf71fc
amaraldo23 <amaraldoo.zhurda@gmail.com>
2025-01-19 00:23:26 +0100

    a

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,42 +4,42 @@ export default class HolbertonCourse {
     if (typeof length !== 'number') throw new Error('Length must be a number');
     if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
       throw new Error('Students must be an array of strings');
-  }
+    }
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
   get name() {
-  return this._name;
+    return this._name;
   }
 
   set name(name) {
     if (typeof name !== 'string') {
-    throw new Error('Name must be a string');
+      throw new Error('Name must be a string');
     }
     this._name = name;
   }
-  
+
   get length() {
     return this._length;
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-    throw new Error('Length must be a number');
-  }
+      throw new Error('Length must be a number');
+    }
     this._length = length;
   }
-  
+
   get students() {
     return this._students;
   }
 
-    set students(students) {
-      if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
-        throw new Error('Students must be an array of strings');
-      }
-      this._students = students;
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new Error('Students must be an array of strings');
     }
+    this._students = students;
+  }
 }
