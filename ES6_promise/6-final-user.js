4313237f485e5096a77c0f0c73bfb9827c794ad9
amaraldo23 <amaraldoo.zhurda@gmail.com>
2025-01-18 23:25:58 +0100

    s

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
 
-  return Promise.allSettled(promises).then((results) => {
-    console.log(results);
-  });
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => (result.status === 'fulfilled'
+      ? { status: result.status, value: result.value }
+      : { status: result.status, value: result.reason.toString() })))
+    .catch((error) => error);
 }
