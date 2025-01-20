60a3f9392fd63ef342f12c13063d2068ac224b70
amaraldo23 <amaraldoo.zhurda@gmail.com>
2025-01-20 18:25:21 +0100

    d

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,14 +2,15 @@ export default function updateStudentGradeByCity(listOfStudents, city, newGrades
   const students = listOfStudents.filter((student) => student.location === city);
 
   const updatedStudent = students.map((student) => {
-    const grades = newGrades?.find((grade) => grade.studentId === student.id);
+	const grades = newGrades?.find((grade) => grade.studentId === student.id);
+	//? -> throws error if array undefined
 
-    const grade = grades ? grades.grade : 'N/A';
+	const grade = grades ? grades.grade : 'N/A';
 
-    return {
-      ...student,
-        grade,
-    };
+	return {
+		...student,
+		grade,
+	};
   });
 
   return updatedStudent;
